b4207c17066d66415c1a4f6c9e021f601db29528
anasnedtech <114829917+anasnedtech@users.noreply.github.com>
2023-08-07 21:43:12 +0200

    test  usign import block and command

--- ecr.tf
+++ ecr.tf
@@ -1,8 +1,13 @@
 # AWS Elastic Container Repository:
-resource "aws_ecr_repository" "springboot_repo" {
+/* resource "aws_ecr_repository" "springboot_repo" {
   name = "users-microservice"
 
   image_scanning_configuration {
     scan_on_push = true
   }
+} */
+
+import {
+  to = aws_ecr_repository.service
+  id = "users-microservice"
 }
--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
-output "ecr_url" {
+/* output "ecr_url" {
   description = "The ECR Repo URL"
   value       = aws_ecr_repository.springboot_repo.repository_url
-}
+} */
 output "cluster_arn" {
   description = "The Cluster ARN"
   value       = aws_ecs_cluster.ecs_cluster.arn
